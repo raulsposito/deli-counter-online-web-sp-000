bffc25a627a7756135e0d6e655b9b3c6d6d1abb1
Raul Sposito <raulsposito@gmail.com>
2019-07-19 13:21:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,3 @@
 # Write your code here.
 katz_deli = []
 #line method should show hteir place in line 
-def line(katz_deli)
-  if katz_deli.size = 0
-  "The line is currently empty."
-else
-  katz_deli 
