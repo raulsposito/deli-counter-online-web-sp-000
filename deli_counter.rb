ccdc16ace859973ed8ff06002bd36e671a199f6c
Raul Sposito <raulsposito@gmail.com>
2019-07-19 13:34:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,9 @@
 # Write your code here.
 katz_deli = []
 #line method should show their place in line 
-def line(numberinline)
+def line(line_method_array)
   line_method_array = []
-  if numberinline.length == 0
+  if line_method_array.length == 0
     puts "The line is currently empty."
   else 
     line_method_array.each.with.index(0) do |name, index|
