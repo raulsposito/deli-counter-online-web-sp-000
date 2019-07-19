7add1e6706b38c6b830bcf7453ba3050983866d7
Raul Sposito <raulsposito@gmail.com>
2019-07-19 13:27:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,21 @@
 # Write your code here.
 katz_deli = []
 #line method should show hteir place in line 
-def line(katz_deli)
-  if katz_deli.size = 0
-  "The line is currently empty."
-else
+def line(numberinline)
+  if numberinline.length == 0
+    puts "The line is currently empty."
+  else 
+    
   
+def line_method(numinline)
+  line_method_array = []
+  if numinline.length == 0
+    puts "The line is currently empty."
+  else
+    line_method_array.each.with_index(0) do |name, index|
+      line_method_array.push("#{index}. #{name}")
+      # push method will push the name into the index 
+    end
+    puts "The line is: #{line_method_array.join(" ")}"
+  end
+end
